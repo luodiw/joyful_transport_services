1e1cc5c036350f324d19b52c60359e2d1fb3f632
Luodi Wang <lwang4319@gmail.com>
2025-07-17 16:53:12 -0500

    new update

--- src/main/java/com/joyful_transport/defs/commuters.java
+++ src/main/java/com/joyful_transport/defs/commuters.java
@@ -1,5 +1,53 @@
 package com.joyful_transport.defs;
 
-public class commuters {
-    
+class commuters {
+	int id;
+	public Stops boardedAt = null;
+	public Stops departedAt = null;
+	String commuterDisplay;
+
+	public commuters(int id) {
+		this.id = id;
+	}
+
+	public void registerCommuter() {
+		centralControlSystem.registerCommuter(this);
+	}
+
+	public void retrieveRouteMap(String sourceStop, String destStop) {
+		commuterDisplay = centralControlSystem.retrieveRouteMap(sourceStop, destStop);
+		this.displayNotifications(commuterDisplay);
+	}
+
+	public void retrieveFare(String sourceStop, String destStop) {
+		commuterDisplay = centralControlSystem.retrieveFare(sourceStop, destStop);
+		this.displayNotifications(commuterDisplay);
+	}
+
+	public void retrieveSchedule(String sourceStop) {
+		commuterDisplay = centralControlSystem.retrieveSchedule(sourceStop);
+		this.displayNotifications(commuterDisplay);
+	}
+
+	public void retrieveRealTimeLocation() {
+		commuterDisplay = centralControlSystem.retrieveRealTimeLocations();
+		this.displayNotifications(commuterDisplay);
+	}
+
+	public void requestToBoardVehicle(bus b) {
+		b.fc.requestToBoardVehicle(this, b);
+	}
+
+	public void requestToDepartVehicle(bus b) {
+		b.fc.requestToDepartVehicle(this, b);
+	}
+
+	public void displayNotifications(String commuterDisplay) {
+		System.out.println(commuterDisplay);
+	}
+	
+	public void requestVan(String s) {
+	centralControlSystem.requestVan(this, s);
+	
+	}
 }
